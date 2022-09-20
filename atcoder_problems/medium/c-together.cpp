2ac34033363e929a74b6266bc2c6b2a0563fc805
toshi201 <aquaconner8@gmail.com>
2022-09-21 08:34:06 +0900

    c-together

--- atcoder_problems/medium/c-together.cpp
+++ atcoder_problems/medium/c-together.cpp
@@ -6,4 +6,15 @@ using namespace std;
 int main() {
     int N; cin >> N;
     vector<int> A(N); for(auto& a: A) { cin >> a; }
+    vector<int> B(2e5, 0);
+
+    for(auto& a: A) {
+        B[a]++;
+        if (a >= 1) {
+            B[a - 1]++;
+        }
+        B[a + 1]++;
+    }
+
+    cout << *std::max_element(B.begin(), B.end()) << endl;
 }
